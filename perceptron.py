6daf8ca21ddcf99755e1bec507585f286ba3d549
tlgimenes <tlgimenes@gmail.com>
2015-03-04 16:10:33 +0100

    Modified and improoved perceptron implementation

--- perceptron.py
+++ perceptron.py
@@ -8,12 +8,6 @@ types_clf = ['Pclass', 'Sex', 'Age'];
 
 ###############################################################################
 
-#Step function
-def heaviside(x):
-    if x > 0:
-        return 1
-    return 0
-
 #train_perceptron: returns a vector (w) for the dot product to predict test data
 def train_perceptron (train_data):
     
@@ -37,21 +31,17 @@ def train_perceptron (train_data):
         errors = 0
 
         for i in range (num_inputs):
-            expected = heaviside(train_data[i]['Survived'])
+            expected = train_data[i]['Survived']
             
             #attempts to predict the output
-            predicted = 0
-            for j in range(num_feats):
-                predicted += w[j]*learning_data[i][j]
+            predicted = np.dot(w, learning_data[i]);
                 
-            predicted = heaviside(predicted)
+            predicted = 1 if predicted > 0 else -1 
             if predicted != expected:
                 errors += 1
             
             #rotate the hyperplane (if prediction was wrong)
-            for j in range(num_feats):
-                w[j] += eta*(expected - predicted)*learning_data[i][j]
-        
+            w += eta * (expected - predicted) * learning_data[i];
        
     rate = 100*errors/num_inputs
     return w
@@ -68,16 +58,6 @@ def to_ndarray(tupple, tupple_type):
 
     return mat;
 
-#predicts output with vector w from training set
-def calc_perceptron(w, data_row):
-    ans = 0
-    
-    #basically we get the sign of the linear combination
-    for i in range (len(data_row)):
-        ans = ans + w[i]*data_row[i]
-    
-    return heaviside (ans);
-        
 
 #Perceptron algorithm: Calls training set and returns the array of 0s and 1s predicting test dataset
 def perceptron (train_data, test_data):
@@ -86,12 +66,12 @@ def perceptron (train_data, test_data):
     w = train_perceptron (train_data)
 
     #gets subset of useful data
-    prediction_data = matrix(test_data[types_clf]).T.getA1();
+    prediction_data = to_ndarray(test_data, types_clf);
     
     #runs the calculation for each test element
     num_inputs = len(prediction_data)
     ans = zeros (num_inputs)
     for i in range(num_inputs):
-        ans[i] = calc_perceptron(w, prediction_data[i])
-    
+        ans[i] = 1 if np.dot(w, prediction_data[i]) > 0 else 0;
+
     return ans
